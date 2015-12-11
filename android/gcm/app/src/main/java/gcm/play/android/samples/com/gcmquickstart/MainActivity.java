5483ace785b13bf39086fb3bfb297a60f6a1caa8
jlvivero <josluivivgar@gmail.com>
2015-12-10 23:47:39 -0800

    hope this works

--- android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/MainActivity.java
+++ android/gcm/app/src/main/java/gcm/play/android/samples/com/gcmquickstart/MainActivity.java
@@ -14,8 +14,6 @@
  * limitations under the License.
  */
 
-
-
 package gcm.play.android.samples.com.gcmquickstart;
 
 import android.content.BroadcastReceiver;
@@ -23,52 +21,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import gcm.play.android.samples.com.gcmquickstart.API.MessApi;
-import gcm.play.android.samples.com.gcmquickstart.Models.GlobalMessage;
-import gcm.play.android.samples.com.gcmquickstart.Models.PostCallback;
-import gcm.play.android.samples.com.gcmquickstart.Models.Users;
-import retrofit.Callback;
-import retrofit.RestAdapter;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
     private static final String TAG = "MainActivity";
 
-    // my variables HERE
-    private Users userList = new Users();
-    private TextView tv;
-
-    String API = "http://test-scala-server.herokuapp.com";
     private BroadcastReceiver mRegistrationBroadcastReceiver;
     private ProgressBar mRegistrationProgressBar;
     private TextView mInformationTextView;
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,92 +86,6 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, RegistrationIntentService.class);
             startService(intent);
         }
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
-
-        tv = (TextView) findViewById(R.id.tv);
-        //OBTAINING THE LIST OF USERS STUFF
-        RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(API).build();
-        //creates an adapter for retrofit with the base url
-
-        MessApi messenger = restAdapter.create(MessApi.class);
-        //creates server for adapter with our get class
-
-        //NOW, we need to call for response
-        //retrofit using gson for JSON-POJO STUFF
-
-        messenger.getFeed(new Callback<Users>() {
-            @Override
-            public void success(Users users, Response response) {
-                userList.copyUsers(users);
-                tv.setText("status: " + userList.getStatus() + "data" + userList.getData());
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                tv.setText(error.getMessage());
-            }
-        });
-
-        //aqui voy a poner los metodos de todos los request, pero obviamente se tienen que arreglar
-        //y poner donde van realmente, por ejemplo este post podria ir en un boton
-
-        /*
-            Anuncio Global con un mensaje
-            Si no estas en el mismo lugar que al principo tienes que declarar un adapter
-            Aqui ya esta declarado arriba para el get de los usuarios
-            Si no pondrias esto:
-            RestAdapter restAdapter2 = new RestAdapter.Builder().setEndpoint(API).build();
-
-         */
-
-        //aqui en vez de "prueba", "JL" pondrias las variables de el modelo Global Message
-        GlobalMessage jsonResponse = new GlobalMessage("prueba", "JL");
-
-
-
-        messenger.postFeed(jsonResponse, new Callback<PostCallback>() {
-            @Override
-            public void success(PostCallback s, Response response) {
-                //aqui pondrias donde quieres que aparesca el mensaje de la respuesta
-                tv.setText("status: " + s.getStatus() + ", message: " + s.getMessage());
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-                tv.setText(error.getMessage());
-            }
-        });
-
-        /*
-            Post directo a una persona
-            Igual si no esta en el mismo lugar tienes que llamar al rest adapter
-            utilizare la misma variable jsonResponse, pero si esta en otro lugar tienes que declararla
-            como arriba, igual con el MessAPI post
-         */
-        //en vez de itzel pondrias una variable con alguno de los usuarios aqui le darias click a algun usuario
-        messenger.postToUser("Itzel", jsonResponse, new Callback<PostCallback>() {
-            @Override
-            public void success(PostCallback postCallback, Response response) {
-                //aqui pondrias lo que quieres que haga la aplicacion despues de mandar el mensaje
-                //en vez de poner tv.setText()
-                tv.setText(postCallback.getStatus());
-            }
-
-            @Override
-            public void failure(RetrofitError error) {
-
-            }
-        });
-
-        /*
-            Notify a todos los usuarios, lo mismo aplica con las cosas que no estoy repitiendo sacalas
-            de los post y get original
-            este solo manda una notificacion asi que solo ocupa el url del usuario
-         */
-        messenger.notifyResponse("Itzel");
-
     }
 
     @Override
@@ -237,43 +122,4 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://gcm.play.android.samples.com.gcmquickstart/http/host/path")
-        );
-        AppIndex.AppIndexApi.start(client, viewAction);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        Action viewAction = Action.newAction(
-                Action.TYPE_VIEW, // TODO: choose an action type.
-                "Main Page", // TODO: Define a title for the content shown.
-                // TODO: If you have web page content that matches this app activity's content,
-                // make sure this auto-generated web page URL is correct.
-                // Otherwise, set the URL to null.
-                Uri.parse("http://host/path"),
-                // TODO: Make sure this auto-generated app deep link URI is correct.
-                Uri.parse("android-app://gcm.play.android.samples.com.gcmquickstart/http/host/path")
-        );
-        AppIndex.AppIndexApi.end(client, viewAction);
-        client.disconnect();
-    }
 }
